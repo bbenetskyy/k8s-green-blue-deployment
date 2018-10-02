e174b68cd62f3ea71bf522cf53ce50b8904accc9
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-10-02 10:27:38 +0200

    Replace with simple WebApi instead of MVC

---
+++ App/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Checker.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public string Get() => "v0.5";
+    }
+}
